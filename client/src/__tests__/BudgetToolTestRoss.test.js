a299c965f6c8fc230a9dcd0e69f662281f3278dc
henderos <henderos@oregonstate.edu>
2025-02-25 20:20:33 -0800

    Revert "Added the preexisting Plaid Integration to the Login Page in the frontend. Also added a .gitignore file to prevent packages from being committed."
    This reverts commit f4504e6c50c6fea31444b656d3d3ddb2830131c5.

--- client/src/__tests__/BudgetToolTestRoss.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import BudgetTool from '../components/BudgetTool';
-
-test('renders budget tool component', () => {
-  render(<BudgetTool />);
-  expect(screen.getByText(/Set Your Budget/)).toBeInTheDocument();
-});
